324fa1bac8fa4b81e46c8a3c713f1a18dc1349a6
Simon Støvring <mail@simonbs.dk>
2022-09-07 08:05:22 +0200

    Moves sleeves to separate section

--- BuildABuddy/Views/Configurator/ShirtConfiguratorView.swift
+++ BuildABuddy/Views/Configurator/ShirtConfiguratorView.swift
@@ -15,28 +15,32 @@ struct ShirtConfiguratorView: View {
     /// The content and behavior of the view.
     var body: some View {
         List {
-            Picker("Front Style", selection: $builder.shirtFrontStyle) {
-                ForEach(Shirt.Front.Style.allCases) { style in
-                    Text(style.title)
-                }
-            }
-            Picker("Front Color", selection: $builder.shirtFrontColor) {
-                ForEach(Shirt.Color.allCases) { color in
-                    Text(color.title)
-                }
-            }
-            Toggle(isOn: $builder.isShirtSleevesEnabled.animation()) {
-                Text("Add Sleeves")
-            }
-            if builder.isShirtSleevesEnabled {
-                Picker("Sleeve Length", selection: $builder.shirtSleeveLength) {
-                    ForEach(Shirt.Sleeve.Length.allCases) { length in
-                        Text(length.title)
+            Section {
+                Picker("Front Style", selection: $builder.shirtFrontStyle) {
+                    ForEach(Shirt.Front.Style.allCases) { style in
+                        Text(style.title)
                     }
                 }
-                Picker("Sleeve Color", selection: $builder.shirtSleeveColor) {
+                Picker("Front Color", selection: $builder.shirtFrontColor) {
                     ForEach(Shirt.Color.allCases) { color in
                         Text(color.title)
+                    }
+                }
+            }
+            Section {
+                Toggle(isOn: $builder.isShirtSleevesEnabled.animation()) {
+                    Text("Add Sleeves")
+                }
+                if builder.isShirtSleevesEnabled {
+                    Picker("Sleeve Length", selection: $builder.shirtSleeveLength) {
+                        ForEach(Shirt.Sleeve.Length.allCases) { length in
+                            Text(length.title)
+                        }
+                    }
+                    Picker("Sleeve Color", selection: $builder.shirtSleeveColor) {
+                        ForEach(Shirt.Color.allCases) { color in
+                            Text(color.title)
+                        }
                     }
                 }
             }
