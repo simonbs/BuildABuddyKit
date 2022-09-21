1770a257bfcc5483a5f3bd5f75a60f94ae2b68b5
Simon Støvring <mail@simonbs.dk>
2022-09-21 17:48:00 +0200

    Adds tutorial on adding tutorials

---
+++ LearningMaterial/Sources/LearningMaterial/Documentation.docc/Tutorials/02-adding-a-tutorial-01-08.swift
@@ -0,0 +1,15 @@
+@Tutorials(name: "BuildABuddyKit") {
+    @Intro(title: "Meet BuildABuddyKit") {
+        Explore BuildABuddyKit through this series of interactive tutorials.
+
+        @Image(source: hero.png, alt: "A collection of buddies generated using the framework")
+    }
+
+    @Chapter(name: "BuildABuddyKit Essentials") {
+        Get started using BuildABuddyKit.
+
+        @Image(source: 01-buildabuddykit-essentials.png, alt: "A character's face outlined with a dashed border.")
+
+        @TutorialReference(tutorial: "doc:<#tutorial name#>")
+    }
+}
