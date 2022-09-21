e62d255f7313bea6c73240be3aa5ddc29ce5a869
Simon Støvring <mail@simonbs.dk>
2022-09-21 17:48:00 +0200

    Adds tutorial on adding tutorials

---
+++ LearningMaterial/Sources/LearningMaterial/Documentation.docc/Tutorials/02-adding-a-tutorial-01-07.swift
@@ -0,0 +1,15 @@
+@Tutorials(name: "BuildABuddyKit") {
+    @Intro(title: "Meet BuildABuddyKit") {
+        Explore BuildABuddyKit through this series of interactive tutorials.
+
+        @Image(source: hero.png, alt: "A collection of buddies generated using the framework")
+    }
+
+    @Chapter(name: "BuildABuddyKit Essentials") {
+        Get started using BuildABuddyKit.
+
+        @Image(source: <#file#>, alt: "<#accessible description#>")
+
+        @TutorialReference(tutorial: "doc:<#tutorial name#>")
+    }
+}
---
+++ LearningMaterial/Sources/LearningMaterial/Documentation.docc/Tutorials/02-adding-a-tutorial-02-10.swift
@@ -0,0 +1,27 @@
+@Tutorial(time: 30) {
+    @Intro(title: "Customizing a Buddy") {
+        This tutorial will guide you through adding BuildABuddyKit to your project and customizing your first buddy.
+    }
+
+    @Section(title: "Create and customize a buddy") {
+        @ContentAndMedia {
+            Create an instance of `Buddy` and customize it.
+
+            @Image(source: 01-customizing-a-buddy-01.png, alt: "A MacBook Pro running Xcode.")
+        }
+
+        @Steps {
+            @Step {
+                Select `ContentView.swift` in the Project navigator.
+
+                @Image(source: <#file#>, alt: "<#accessible description#>")
+            }
+
+            @Step {
+                <#text#>
+
+                @Code(name: "<#display name#>", file: <#filename.swift#>)
+            }
+        }
+    }
+}
