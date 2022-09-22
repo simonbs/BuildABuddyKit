0803795c274b563381726fa5adc5806bb029b452
Simon Støvring <mail@simonbs.dk>
2022-09-22 14:22:12 +0200

    Adds Linking to Resources tutorial

---
+++ LearningMaterial/Sources/LearningMaterial/Documentation.docc/Tutorials/02-linking-to-resources-03-03.swift
@@ -0,0 +1,30 @@
+@Tutorials(name: "BuildABuddyKit") {
+    @Intro(title: "Meet BuildABuddyKit") {
+        Explore BuildABuddyKit through this series of interactive tutorials.
+
+        @Image(source: hero.png, alt: "A collection of buddies generated using the framework")
+    }
+
+    @Chapter(name: "BuildABuddyKit Essentials") {
+        Get started using BuildABuddyKit.
+
+        @Image(source: 01-buildabuddykit-essentials.png, alt: "A character's face outlined with a dashed border.")
+
+        @TutorialReference(tutorial: "doc:Customizing-a-Buddy")
+        @TutorialReference(tutorial: "doc:Rendering-a-Buddy-to-an-Image")
+    }
+
+    @Resources {
+        Explore more resources for learning about BuildABuddyKit.
+
+        @Documentation(destination: "https://github.com/simonbs/BuildABuddyKit") {
+            Browse and search detailed API documentation.
+
+            - [Documentation](https://github.com/simonbs/BuildABuddyKit/blob/main/README.md#-documentation)
+        }
+
+        @Forums(destination: "https://github.com/simonbs/BuildABuddyKit") {
+            Create pull requests and discuss issues.
+        }
+    }
+}
